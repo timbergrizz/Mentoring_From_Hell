8613ad53d28358af3db8eacb9f68a5c34493529a
Jong Wook Kang <50934118+jisooyein@users.noreply.github.com>
2020-04-30 03:46:54 +0900

    apply file upload logic into board

--- Web/building_board_service/src/create.php
+++ Web/building_board_service/src/create.php
@@ -11,10 +11,14 @@ require_once("lib/connect.php");
   </head>
   <body>
     <h1>Create New Article</h1>
-    <form action="process/create.php" method="POST">
+    <form action="process/create.php" method="POST"  enctype="multipart/form-data">
         <p><input type="hidden" name="user_id" value="<?=$user_id ?>"></p>
         <p><input type="text" name="title" placeholder="title"></p>
         <p><textarea name="content" placeholder="content"></textarea></p>
+        <p>
+          <input type="file" name="file_upload" id="file_upload">
+          <input type="submit" value="Upload Image" name="submit"> 
+        </p>
         <p><input type="submit"></p>
     </form>
   </body>
