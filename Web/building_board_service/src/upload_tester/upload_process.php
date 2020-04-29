96fb0649fae35891d6fd56a4fd6a069f880aa7b7
Jong Wook Kang <50934118+jisooyein@users.noreply.github.com>
2020-04-30 03:22:57 +0900

    undefined variable?

--- Web/building_board_service/src/upload_tester/upload_process.php
+++ Web/building_board_service/src/upload_tester/upload_process.php
@@ -2,8 +2,9 @@
 $target_dir = "uploads/"; // 파일 업로드 되는 위치
 $target_flie = $target_dir . basename($_FILES["file_upload"]["name"]); // 파일 업로드되는 경로 + 파일
 $upload_checker = 1; // 업로드 유효성 확인용
+
 /*
-if($_FILES["fo;e_upload"]["name"] == NULL){
+if($_FILES["file_upload"]["name"] == NULL){
     echo "There is no file to upload";
 }
 
@@ -15,13 +16,17 @@ if(file_exists($target_flie)){ // 파일명 중복 확인.
 if ($upload_checker == 0) { // 파일 업로드 유효성 검사 실패. 파일 업로드가 이루어지지 않음
     echo "Sorry, your file was not uploaded.";
 } else { // 파일 업로드가 이루어짐.
+
     if (move_uploaded_file($_FILES["file_upload"]["name"], $target_file)) {
-    print("file successfully uploaded");
+    
+        print("file successfully uploaded");
 
     } else {
+    
         echo "there is an error";
         ini_set("display_errors", 1);
         error_reporting(E_ALL);
+
     }
 }
 
