95ba7d84e7f64beb5425f11f643a8f118a25b21d
Jong Wook Kang <50934118+jisooyein@users.noreply.github.com>
2020-04-30 02:50:43 +0900

    disable file checker to test

--- Web/building_board_service/src/upload_tester/upload_process.php
+++ Web/building_board_service/src/upload_tester/upload_process.php
@@ -2,7 +2,7 @@
 $target_dir = "uploads/"; // 파일 업로드 되는 위치
 $target_flie = $target_dir . basename($_FILES["file_upload"]["name"]); // 파일 업로드되는 경로 + 파일
 $upload_checker = 1; // 업로드 유효성 확인용
-
+/*
 if($_FILES["fo;e_upload"]["name"] == NULL){
     echo "There is no file to upload";
 }
@@ -11,7 +11,7 @@ if(file_exists($target_flie)){ // 파일명 중복 확인.
     echo "file name is already used.";
     $upload_checker = 0;
 }
-
+*/
 if ($uploadOk == 0) { // 파일 업로드 유효성 검사 실패. 파일 업로드가 이루어지지 않음
     echo "Sorry, your file was not uploaded.";
 } else { // 파일 업로드가 이루어짐.
