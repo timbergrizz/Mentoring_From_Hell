39e627c66144446ebe51f3283be27891367a355e
Jong Wook Kang <50934118+jisooyein@users.noreply.github.com>
2020-04-30 03:25:23 +0900

    variable name fix

--- Web/building_board_service/src/upload_tester/upload_process.php
+++ Web/building_board_service/src/upload_tester/upload_process.php
@@ -1,6 +1,6 @@
 <?php
 $target_dir = "uploads/"; // 파일 업로드 되는 위치
-$target_flie = $target_dir . basename($_FILES["file_upload"]["name"]); // 파일 업로드되는 경로 + 파일
+$target_file = $target_dir . basename($_FILES["file_upload"]["name"]); // 파일 업로드되는 경로 + 파일
 $upload_checker = 1; // 업로드 유효성 확인용
 
 /*
@@ -8,7 +8,7 @@ if($_FILES["file_upload"]["name"] == NULL){
     echo "There is no file to upload";
 }
 
-if(file_exists($target_flie)){ // 파일명 중복 확인.
+if(file_exists($target_file)){ // 파일명 중복 확인.
     echo "file name is already used.";
     $upload_checker = 0;
 }
