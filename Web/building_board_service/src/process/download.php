647497a6de05104b43f3daf84a1f5d194eb0e22a
Jong Wook Kang <50934118+jisooyein@users.noreply.github.com>
2020-05-05 04:23:27 +0900

    downloader filename check

---
+++ Web/building_board_service/src/process/download.php
@@ -0,0 +1,19 @@
+<?
+require_once("../lib/connect.php");
+
+$filename = $_GET["filename"];                      
+$file_dir = "../uploads/".$filename;  
+
+
+die($file_dir);
+
+header('Content-Type: application/x-octetstream');
+header('Content-Length: '.filesize($file_dir));
+header('Content-Disposition: attachment; filename='.$reail_filename);
+header('Content-Transfer-Encoding: binary');
+
+$fp = fopen($file_dir, "r");
+fpassthru($fp);
+fclose($fp);
+
+?>
